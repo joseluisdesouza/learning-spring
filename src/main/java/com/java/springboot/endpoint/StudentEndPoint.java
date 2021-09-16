b881bb7fbf32fc9949ff793a30b5197b13ed010e
josesouza <joseluisdesouzaa@gmail.com>
2021-09-16 18:08:30 -0300

    adjustment

--- src/main/java/com/java/springboot/endpoint/StudentEndPoint.java
+++ src/main/java/com/java/springboot/endpoint/StudentEndPoint.java
@@ -3,20 +3,31 @@ package com.java.springboot.endpoint;
 import com.java.springboot.domain.Student;
 import com.java.springboot.error.CustomErrorType;
 import com.java.springboot.util.DateUtil;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("students")
+@Log4j2
 public class StudentEndPoint {
-
+    //localhost:8080/students
     @Autowired
     private DateUtil dateUtil;
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> save(@RequestBody Student student) {
+        Student.studentList.add(student);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> listAll() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
 
@@ -28,12 +39,6 @@ public class StudentEndPoint {
         if (index == -1)
             return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<?> save(@RequestBody Student student) {
-        Student.studentList.add(student);
-        return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
